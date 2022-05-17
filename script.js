6658751c00bcfd28bfaa3dce93b04f4f71cfb033
akifzsche <akif.tonmaister58@gmail.com>
2022-05-18 01:49:00 +0300

    anime.js and new project added (markdown)

---
+++ script.js
@@ -0,0 +1,22 @@
+var textWrapper = document.querySelector('.ml6 .letters');
+textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
+
+anime.timeline({ loop: false })
+    .add({
+        targets: '.ml6 .letter',
+        translateY: ["1.1em", 0],
+        translateZ: 0,
+        duration: 750,
+        delay: (el, i) => 50 * i
+    })
+
+var textWrapper = document.querySelector('.ml10 .letters');
+textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
+
+anime.timeline({ loop: false })
+    .add({
+        targets: '.ml10 .letter',
+        rotateY: [-90, 0],
+        duration: 800,
+        delay: (el, i) => 7 * i
+    })
